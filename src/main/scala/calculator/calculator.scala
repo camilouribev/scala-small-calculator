b39fcbd861e13bf2f8a48de08c6c90c805d29d7b
camilo-uribe <camilo.uribev@sofka.com.co>
2024-02-12 15:34:33 -0500

    Test small quotation project

---
+++ src/main/scala/calculator/calculator.scala
@@ -0,0 +1,29 @@
+package org.sofka
+package calculator
+
+object calculator extends App {
+  val iva = 0.19
+  val ipoConsumo = 0.16
+
+  def calculateIVA(itemValue: Double): Double = {
+    itemValue * iva
+  }
+
+  def calculateIpoConsumo(itemValue: Double): Double = {
+    itemValue * ipoConsumo
+  }
+
+  def quotation(items: List[Double]): Unit = {
+    val brute = items.sum
+    val totalIVA = calculateIVA(brute)
+    val totalIpoConsumo = calculateIpoConsumo(brute)
+    val total = brute + totalIVA + totalIpoConsumo
+    println(s"Valor bruto: $brute")
+    println(s"Valor iva : $totalIVA")
+    println(s"Valor total: $total")
+
+  }
+
+  quotation(List(20,40,677))
+
+}
